958e99438f20fdee0c11802fe80ef84316c4f70a
nahhomi <lucytomachambilla@gmail.com>
2017-05-11 21:39:50 -0500

    parcial1

---
+++ parcial1/scripts.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>Scripts PHP <?php date_default_timezone_set('Europe/Paris');echo date("h:i:s");?></title>
+	<meta charset="utf-8">
+</head>
+<body>
+	<h1> Scripts<?php echo "PHP";?></h1>
+	<p> Un Scripts PHP esta contenida entre las etiquetas<b><code>&Lt;&#63;php</code></b> y <b><code>&#63;&gt;</code></b>.
+	</p><?php echo date("h:i:s");?>
+</body>
+</html>
